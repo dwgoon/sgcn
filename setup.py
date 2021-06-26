f8c0c215b0f0306f3784af3abf4036c8168695a8
Daewon Lee <daewon4you@gmail.com>
2021-06-26 09:20:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,16 @@ from setuptools import setup, find_packages
 setup(
     name='sgcn',
     version="0.0.1",
-    description='Steganography for Complex Networks',
+    description='Steganography of Complex Networks',
     author='Daewon Lee',
     author_email='daewon4you@gmail.com',
     packages=find_packages(),
     install_requires=[
         'numpy',
-        'networkx',          
+        'scipy',
+        'pandas',
+        'networkx',
+        'bitstring',
+        'tqdm'
     ]
 )
